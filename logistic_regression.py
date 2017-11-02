5bd0724a5498b7c28e0973e0aef3d918559ab765
Zhaoqi(Zack) Xu <zhaoq.xu@gmail.com>
2017-11-02 00:22:48 -0400

    deadcode deleted

--- logistic_regression.py
+++ logistic_regression.py
@@ -9,16 +9,13 @@ y = np.loadtxt("train_y.csv", delimiter=",")
 print("train_y is loaded")
 z = np.loadtxt("test_x.csv", delimiter=",")
 print("test_x is loaded")
-# print(x.shape)
-# x = x.reshape(-1, 64, 64)   # reshape
-# y = y.reshape(-1, 1)
-# z = z.reshape(-1, 64, 64)
-# print(x.shape)
+
 clf = LogisticRegression(multi_class="multinomial", solver="lbfgs")
 clf.fit(x, y)   # x[nsample, nfeature]
 print("Your logistic classifier is ready")
 predictions = clf.predict(z)
 print("Writing results into predictions_lr.csv...")
+
 with open('predictions_lr.csv', 'w', newline='') as f:
     fieldnames = ["Id", "Label"]
     writer = csv.DictWriter(f, fieldnames=fieldnames)
